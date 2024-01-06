04dffc1431280bb3a307981f719b3c4f835cd692
framework <madhvikrana@gmail.com>
2024-01-06 22:12:05 +0545

    4th commit

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -4,16 +4,16 @@ import "./login.scss"
 const Login = () => {
   return (
     <div className="login">
-        <div className='card'>
-            <div className='left'>
+        <div className="card">
+            <div className="left">
                 <h1>Hello World.</h1>
                 <p>
-                    This is the login page fr my first social media app.
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </p>
                 <span>Dont you have an account?</span>
                 <button>Register</button>
             </div>
-            <div className='right'>
+            <div className="right">
                 <h1>Login</h1>
                 <form>
                     <input type="text" placeholder="Username"/>
--- src/Pages/register/Register.jsx
+++ src/Pages/register/Register.jsx
@@ -3,8 +3,27 @@ import "./register.scss"
 
 const Register = () => {
   return (
-    <div className='register'>Register
+    <div className="login">
+        <div className="card">
+            <div className="left">
+                <h1>Hello World.</h1>
+                <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </p>
+                <span>Dont you have an account?</span>
+                <button>Register</button>
+            </div>
+            <div className="right">
+                <h1>Login</h1>
+                <form>
+                    <input type="text" placeholder="Username"/>
+                    <input type="password" placeholder="Password"/>
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
     </div>
+      
   )
 }
 
